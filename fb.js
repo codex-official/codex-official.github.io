9e797aae5a417592a11487333e0bfb495c64f5b7
codex-official <77504210+codex-official@users.noreply.github.com>
2024-03-26 00:59:50 +0530

    Update fb.js

--- fb.js
+++ fb.js
@@ -8,7 +8,7 @@ import {getDatabase, ref, push, onValue, set, get, child} from "https://www.gsta
 
 
 const firebaseConfig = {
-    apiKey: ${{secrets.API_KEY }},
+    apiKey: ${{secrets.api_key }},
     authDomain: "fir-test-1b571.firebaseapp.com",
     databaseURL: "https://fir-test-1b571-default-rtdb.firebaseio.com",
     projectId: "fir-test-1b571",
